7bae7a795b15355dda3c9aba0a157172f6e1c922
Dmytro Farbun <dimofarb@gmail.com>
2025-12-20 17:51:40 +0200

    feat: add profile page

---
+++ app/(private routes)/profile/page.tsx
@@ -0,0 +1,57 @@
+import css from './ProfilePage.module.css';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Profile page",
+  description: "User profile page with personal information",
+  metadataBase: "https://09-auth-five-nu.vercel.app/",
+  openGraph: {
+    title: "Profile page",
+    description: "User profile page with personal information",
+    url: "https://09-auth-five-nu.vercel.app/",
+    images: [
+      {
+        url: "https://09-auth-five-nu.vercel.app/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Notehub - Profile page"
+      }
+    ]
+  }
+};
+
+const Profile = () => {
+  const username = 'your_username';
+  const email = 'your_email@example.com';
+
+  return (
+    <main className={css.mainContent}>
+      <div className={css.profileCard}>
+        <div className={css.header}>
+          <h1 className={css.formTitle}>Profile Page</h1>
+          <Link href="/" className={css.editProfileButton}>
+            Edit Profile
+          </Link>
+        </div>
+        <div className={css.avatarWrapper}>
+          <Image
+            className={css.avatar}
+            src="Avatar"
+            alt="User Avatar"
+            width={120}
+            height={120}
+          />
+        </div>
+        <div className={css.profileInfo}>
+          <p>Username: {username}</p>
+          <p>Email: {email}</p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Profile;
