f32d08d37bb2dc6a0670584c721de45ccde5bf0c
Dmytro Farbun <dimofarb@gmail.com>
2025-12-20 23:52:30 +0200

    feat: add sign-in page with login logic

---
+++ app/(auth routes)/sign-in/page.tsx
@@ -0,0 +1,76 @@
+'use client';
+import css from './SignInPage.module.css';
+
+import { LoginRequest, login } from '@/lib/api/clientApi';
+
+import { isAxiosError } from 'axios';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+const SignIn = () => {
+  const router = useRouter();
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      const formValues = Object.fromEntries(formData) as LoginRequest;
+      const res = await login(formValues);
+
+      if (res) {
+        router.push('/profile');
+      } else {
+        setError('Invalid email or password');
+      }
+    } catch (error) {
+      if (isAxiosError(error)) {
+        setError(
+          error?.response?.data?.error ??
+            error?.message ??
+            'Oops.. something went wrong'
+        );
+      } else {
+        setError('Oops.. something went wrong');
+      }
+    }
+  };
+
+  return (
+    <main className={css.mainContent}>
+      <form action={handleSubmit} className={css.form}>
+        <h1 className={css.formTitle}>Sign In</h1>
+
+        <div className={css.formGroup}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <button type="submit" className={css.submitButton}>
+            Log In
+          </button>
+        </div>
+
+        {error && <p className={css.error}>{error}</p>}
+      </form>
+    </main>
+  );
+};
+
+export default SignIn;
