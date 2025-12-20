c548a185aa5def69a10de3debf196961bfe1a32b
Dmytro Farbun <dimofarb@gmail.com>
2025-12-20 19:41:25 +0200

    feat: add SighUp page

---
+++ app/(auth routes)/sign-up/page.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import css from './SignUpPage.module.css';
+import { register } from '@/lib/api/clientApi';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+function SignUp() {
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(null);
+
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    try {
+      await register({ email, password });
+      router.push('/profile');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <main className={css.mainContent}>
+      <h1 className={css.formTitle}>Sign Up</h1>
+      <form onSubmit={handleSubmit} className={css.form}>
+        <div className={css.formGroup}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="text"
+            name="email"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="text"
+            name="password"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.actions}>
+          <button type="submit" className={css.submitButton}>
+            Register
+          </button>
+        </div>
+
+        {error && <p className={css.error}>{error}</p>}
+      </form>
+    </main>
+  );
+}
+
+export default SignUp;
