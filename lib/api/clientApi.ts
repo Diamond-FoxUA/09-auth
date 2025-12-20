f8145e836aed631b875b07e79848bf386ad80e58
Dmytro Farbun <dimofarb@gmail.com>
2025-12-20 19:40:54 +0200

    chore(api): add register API client

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,8 +1,20 @@
+import api from '@/lib/api/api';
+
+import { User } from '@/types/user';
+
+// register
+export async function register(payload: {
+  email: string;
+  password: string;
+}): Promise<User> {
+  const { data } = await api.post<User>("/auth/register", payload);
+  return data;
+}
+
 // fetchNotes
 // fetchNoteById
 // createNote
 // deleteNotes
-// register
 // login
 // logout
 // checkSession
