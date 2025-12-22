81cc7337c2f60ef645ec48ee508fb67e5010d236
Dmytro Farbun <dimofarb@gmail.com>
2025-12-22 19:22:54 +0200

    feath(auth): add AuthProvider to revalidate session on private routes

---
+++ components/AuthProvider/AuthProvider.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { ReactNode, useEffect } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
+
+import { useAuthStore } from '@/lib/store/authStore';
+import { getMe } from '@/lib/api/clientApi';
+const privateRoutes = ['/profile', '/notes'];
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const setUser = useAuthStore(store => store.setUser);
+  const clearIsAuthenticated = useAuthStore(
+    store => store.clearIsAuthenticated
+  );
+
+  const isPrivateRoute = privateRoutes.some(route =>
+    pathname.startsWith(route)
+  );
+
+  const {
+    data: user,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['me'],
+    queryFn: getMe,
+    enabled: isPrivateRoute,
+    retry: false,
+  });
+
+  useEffect(() => {
+    if (isPrivateRoute) return;
+
+    if (user) {
+      setUser(user);
+      return;
+    }
+
+    if (isError) {
+      clearIsAuthenticated();
+      router.replace('/sign-in');
+    }
+  }, [isPrivateRoute, user, isError, setUser, clearIsAuthenticated, router]);
+
+  if (isPrivateRoute && isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return <>{children}</>;
+}
