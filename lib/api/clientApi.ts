490b29c6de2ac7c77d81325b96492665b9bc50ee
Dmytro Farbun <dimofarb@gmail.com>
2025-12-23 16:11:11 +0200

    feat(api): add getMe and updateMe profile requests

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,3 +1,4 @@
+import { api } from '@/app/api/api';
 import nextServer from '@/lib/api/api';
 
 import { User } from '@/types/user';
@@ -6,23 +7,23 @@ import { User } from '@/types/user';
 export type RegisterRequest = {
   email: string;
   password: string;
-}
+};
 
 export const register = async (data: RegisterRequest) => {
-  const res = await nextServer.post<User>("/auth/register", data);
+  const res = await nextServer.post<User>('/auth/register', data);
   return res.data;
-}
+};
 
 // login
 export type LoginRequest = {
   email: string;
   password: string;
-}
+};
 
 export const login = async (data: LoginRequest) => {
-  const res = await nextServer.post<User>("/auth/login", data);
+  const res = await nextServer.post<User>('/auth/login', data);
   return res.data;
-}
+};
 
 // logout
 export const logout = async (): Promise<void> => {
@@ -31,12 +32,22 @@ export const logout = async (): Promise<void> => {
 
 // getMe
 export const getMe = async (): Promise<User> => {
-  const response = await nextServer.get<User>('/users/me');
+  const response = await api.get<User>('/users/me');
   return response.data;
-}
+};
+
+// updateMe
+type UpdateMeProps = {
+  username: string;
+};
+
+export const updateMe = async (data: UpdateMeProps): Promise<User> => {
+  const response = await nextServer.patch<User>('/users/me', data);
+  return response.data;
+};
+
 // fetchNotes
 // fetchNoteById
 // createNote
 // deleteNotes
 // checkSession
-// updateMe
