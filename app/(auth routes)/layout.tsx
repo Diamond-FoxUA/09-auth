74a2af92ce9f2b97f9b759ce01c5632736c6f565
Dmytro Farbun <dimofarb@gmail.com>
2025-12-26 19:15:02 +0200

    fox(auth): refresh router on AuthLayout mount

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,9 +1,17 @@
-import { ReactNode } from "react";
+'use client';
+import { ReactNode, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 interface AuthProps {
   children: ReactNode;
 }
 
 export default function AuthLayout({ children }: AuthProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.refresh();
+  }, [router]);
+
   return children;
 }
